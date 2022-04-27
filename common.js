87a0fe499e43aadfd1e323c41028eb69213aa301
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2022-04-27 17:54:04 +0900

    newin 2가지 완료, 셀렉트 1단 완료

--- common.js
+++ common.js
@@ -4,6 +4,7 @@ window.addEventListener("resize", () => {
 })
 
 window.addEventListener("DOMContentLoaded", () => {
+  // pc로 켜진 html은 1360px 아래로 떨어지지 않는다.
   if (document.body.offsetWidth < 1360) {
     document.body.classList.add("ver_mob");
   } else {
